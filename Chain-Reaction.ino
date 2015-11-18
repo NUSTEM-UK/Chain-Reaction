3452a2ab93c395060cde8c18da316ac8f2fe0a31
Jonathan Sanderson <jonathan.sanderson@northumbria.ac.uk>
2015-11-18 15:38:27 +0000

    FSR & LDR; Latching and threshold
    Considerable update. Next step is to refactor inputs so we’re dealing
    with just one input variable in the loop, and to refactor serial output
    similarly.

--- Chain-Reaction.ino
+++ Chain-Reaction.ino
@@ -2,15 +2,17 @@
 #include <Servo.h>
 
 // Configure operation mode
-String input = "LDR";       // Not currently used
-String output = "SERVO";    // set to MOTOR or SERVO. Not currently used.
-String mode = "THRESHOLD"; // set to THRESHOLD or CONTINUOUS
+String input = "FSR";          // LDR or FSR
+String output = "SERVO";       // set to MOTOR or SERVO. Not currently used.
+String mode = "THRESHOLD";     // set to THRESHOLD or CONTINUOUS
+String behaviour = "RESET"; // set to LATCHING or RESET
 
 // Configure value for threshold mode
-const int threshold = 90;
+const int threshold = 10;
 
 // Configure input
-int ldrPin = 0; // analog pin to which the LDR is connected
+int ldrAnalogPin = 0; // analog pin to which the light-dependent resistor is connected
+int fsrAnalogPin = 1; // pin to which force-sensitive resistor is connected
 int ledPin1 = 13; // pin 13 is mirrored to the onboard LED
 
 // Configure servo
@@ -32,31 +34,53 @@ void setup() {
 void loop() {
 
   // Read the light level from the LDR...
-  int lightLevel = analogRead(ldrPin);
-  
+  int lightLevel = analogRead(ldrAnalogPin);
+
   // ...and map the result to a range the servo can handle
   // you may need to tinker with these values. Here, we're 
   // mapping bright office light to servo angles (0..180 degrees)
   lightLevel = map(lightLevel, 100, 800, 0, 180);
   lightLevel = constrain(lightLevel, 0, 180);     // Make really sure it's within that range
 
+  // Read the value from the FSR...
+  int forceLevel = analogRead(fsrAnalogPin);
+  forceLevel = map(forceLevel, 0, 1023, 0, 180);
+  forceLevel = constrain(forceLevel, 0, 180);
+
+
   if ( mode == "THRESHOLD" ) {
-    // We're looking for a comparison to the threshold value
-    if ( lightLevel > threshold ) {
-      // threshold reached, trigger the servo action
-      myServo1.write(angleTriggered);
-    } else {
-      // threshold not reached, return servo to rest
-      myServo1.write(angleRest);
-    }
+    if ( input == "LDR" ) {
+      if ( lightLevel > threshold ) {
+        // threshold reached, trigger the servo action
+        myServo1.write(angleTriggered);
+      } else {
+        // threshold not reached, return servo to rest
+        if ( behaviour == "RESET") {myServo1.write(angleRest); };
+      }
+    } else if (input == "FSR") {
+      if ( forceLevel > threshold ) {
+        myServo1.write(angleTriggered);
+        } else {
+          if ( behaviour == "RESET") { myServo1.write(angleRest); };
+        }
+      }
   } else {
     // We're working continuously, so update the output directly
-    myServo1.write(lightLevel);
+    if ( input == "LDR") { myServo1.write(lightLevel); };
+    if ( input == "FSR") { myServo1.write(forceLevel); };
   }
 
   // Output lightLevel to serial for debug purposes
-  Serial.print("lightLevel = ");
-  Serial.println(lightLevel);
+  if ( input == "LDR" ) {
+    Serial.print("lightLevel = ");
+    Serial.println(lightLevel);
+  }
+  if ( input == "FSR" ) {
+    Serial.print("forceLevel= ");
+    Serial.println(forceLevel);
+  }
+
+  
 
   // Flash the LED to prove things are working. Comment out to speed up the loop.
   // blink(100);
