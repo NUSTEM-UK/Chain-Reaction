d8b07b73ebe7c93eac22abbc9825c63f5f968065
Jonathan Sanderson <jonathan.sanderson@northumbria.ac.uk>
2015-11-19 11:56:38 +0000

    Distance sensor added

--- Chain-Reaction.ino
+++ Chain-Reaction.ino
@@ -2,19 +2,20 @@
 #include <Servo.h>
 
 // Configure operation mode
-String input = "FLEX";					// LDR, FSR, FLEX
+String input = "DISTANCE";		// LDR, FSR, FLEX, DISTANCE
 String output = "SERVO";			// set to MOTOR or SERVO. Not currently used.
-String mode = "CONTINUOUS";		// set to THRESHOLD or CONTINUOUS
+String mode = "THRESHOLD";		// set to THRESHOLD or CONTINUOUS
 String behaviour = "RESET";		// set to LATCHING or RESET
 
 // Configure value for threshold mode
-const int threshold = 10;
+const int threshold = 90;
 
 // Configure inputs
-const int ldrAnalogPin = 0;		// pin to which the light-dependent resistor is connected
-const int fsrAnalogPin = 1;		// pin to which force-sensitive resistor is connected
-const int flexAnalogPin = 2;	// pin to which flex sensor is connected
-const int ledPin1 = 13; 			// use for diagnostic flashing LED. Pin 13 is mirrored to the onboard LED
+const int ldrAnalogPin = 0;				// pin to which the light-dependent resistor is connected
+const int fsrAnalogPin = 1;				// pin to which force-sensitive resistor is connected
+const int flexAnalogPin = 2;			// pin to which flex sensor is connected
+const int distanceAnalogPin = 3; 	// pin to which distance sensor is connected
+const int ledPin1 = 13; 					// use for diagnostic flashing LED. Pin 13 is mirrored to the onboard LED
 
 // Configure servo
 int servoPin1 = 9;
--- functions.ino
+++ functions.ino
@@ -23,6 +23,9 @@ int updateSensors() {
 	} else if ( input == "FLEX" ) {
 		sensorReading = analogRead(flexAnalogPin);
 		sensorReading = map(sensorReading, 720, 850, 0, 180);
+	} else if ( input == "DISTANCE" ) {
+		sensorReading = analogRead(distanceAnalogPin);
+		sensorReading = map(sensorReading, 0, 1023, 0, 180);
 	} else {
 		sensorReading = 88; // If all you see in the serial window is this, we fell through the cascade above
 	}
